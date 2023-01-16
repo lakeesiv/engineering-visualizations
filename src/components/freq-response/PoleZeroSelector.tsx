c6c10f40e06ad08776140129d7908f9f7b542b7d
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-01-16 16:09:25 +0000

    selectors

--- src/components/freq-response/PoleZeroSelector.tsx
+++ src/components/freq-response/PoleZeroSelector.tsx
@@ -54,11 +54,11 @@ const PoleZeroSelector = () => {
 
   return (
     <>
-      <div>
+      <div className="w-full grid place-items-center pt-2">
         <button
           type="button"
           onClick={openModal}
-          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          className=" bg-black text-2xl  px-4text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 w-[80%] py-8 rounded-2xl"
         >
           Add/Edit Pole/Zero
         </button>
