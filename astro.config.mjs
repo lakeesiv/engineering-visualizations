9feccba11acb78774c61dd01abd5792c987ca002
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-01-16 14:37:01 +0000

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), partytown(), sitemap(), compress()]
 });
