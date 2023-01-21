5f2518e3ae8f755562b525c5fb7b12a05abb4d7c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-01-21 14:20:46 +0000

    styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        b: {
+          900: "#0a0a0a",
+          800: "#141414",
+        },
+      },
+    },
   },
   plugins: [],
 };
